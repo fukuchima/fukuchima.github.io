7f9603377054e7a5a2276c16cfbe6476fd511fd8
fukuchima <masayuki.fukuchi@grapecity.com>
2014-08-26 12:37:26 +0900

    Revert "d2h sample"
    This reverts commit a43f1b4e374b728e256bb077aa35d8fcce8434e3.

--- dotinstall/chromeEx/background.js
+++
@@ -1,8 +0,0 @@
-chrome.browserAction.setBadgeText({"text":"100"})
-
-chrome.browserAction.onClicked.addListener(function(){
-	chrome.tabs.executeScript(null,{
-		"code":"document.body.style.backgroundColor='green'"
-	});
-
-});
